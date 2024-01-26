eae674519fdc9923623716fe1f446d1b920505d1
NinoNogic <11751305+nogicn@users.noreply.github.com>
2024-01-26 23:46:04 +0100

    odvajanje funkcija u zasebni file

--- controllers/wordcontroller/wordcontroller.go
+++ controllers/wordcontroller/wordcontroller.go
@@ -14,72 +14,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func createWords(c *fiber.Ctx, idInt int) error {
-	words, err := worddb.GetAllWordsNotInUserWord(db.DB, c.Locals("user_id").(int))
-	if err != nil {
-		return fiber.NewError(500, "Database error")
-	}
-	////fmt.Println("rijeci", words)
-	for _, word := range words {
-		if word.DictionaryID != idInt {
-			continue
-		}
-		currentDate := time.Now().Add(-time.Hour * 24 * 30)
-		var trueword = userworddb.UserWord{
-			UserID:       c.Locals("user_id").(int),
-			WordID:       word.ID,
-			Active:       1,
-			Delay:        0,
-			LastAnswered: currentDate.Format("2006-01-02 15:04:05"),
-		}
-		err := userworddb.CreateUserWord(db.DB, &trueword)
-		if err != nil {
-			return fiber.NewError(500, "Greška kod stvaranja riječi!")
-		}
-	}
-	return nil
-}
-
-func setActiveQuestion(activequestion *activequestiondb.ActiveQuestion, c *fiber.Ctx, idInt int, typeOf int) error {
-	tmpActive := activequestiondb.ActiveQuestion{}
-
-	// delete old active question
-	if *activequestion != (activequestiondb.ActiveQuestion{}) {
-		err := activequestiondb.DeleteActiveQuestionByUserID(db.DB, c.Locals("user_id").(int))
-		if err != nil {
-			return fiber.NewError(500, "Greška kod brisanja aktivne riječi!")
-		}
-	}
-
-	userWords, _, err := userworddb.GetViableWordsForUserForDictionary(db.DB, c.Locals("user_id").(int), idInt)
-	if err != nil {
-		return fiber.NewError(500, "Greška kod dohvaćanja riječi! nema rijeci za dict")
-	}
-
-	if len(userWords) < 3 {
-		return fiber.NewError(404, "Nema više riječi za učenje!")
-	}
-	//fmt.Println("userWords", userWords)
-	// set random word as active question
-	rand.Seed(time.Now().UnixNano())
-	random := rand.Intn(len(userWords))
-	tmpActive.WordID = userWords[random].WordID
-	tmpActive.UserID = c.Locals("user_id").(int)
-	if typeOf > 4 {
-		typeOf = 1
-	}
-	tmpActive.Type = typeOf
-	err = activequestiondb.SetActiveQuestion(db.DB, &tmpActive)
-	if err != nil {
-		//fmt.Println("AAAAAAAAAAAAAAA")
-		return fiber.NewError(404, "Greška kod stvaranja aktivne riječi!")
-	}
-	tmpActive, _ = activequestiondb.GetActiveQuestionByUserID(db.DB, c.Locals("user_id").(int))
-	//fmt.Println("tmpActive\n", tmpActive)
-	*activequestion = tmpActive
-	return nil
-}
-
 func LearnSession(c *fiber.Ctx) error {
 	id := c.Params("id")
 	idInt, err := strconv.Atoi(id)
@@ -89,7 +23,7 @@ func LearnSession(c *fiber.Ctx) error {
 
 	activequestion, activeerr := activequestiondb.GetActiveQuestionByUserID(db.DB, c.Locals("user_id").(int))
 	if activequestion == (activequestiondb.ActiveQuestion{}) {
-		////fmt.Println("No active question")
+
 		err := createWords(c, idInt)
 		if err != nil {
 			return c.Status(404).Render("forOfor", fiber.Map{"status": "500", "errorText": err, "link": "/dashboard"})
@@ -129,16 +63,16 @@ func LearnSession(c *fiber.Ctx) error {
 
 	switch activequestion.Type {
 	case 1:
-		//fmt.Println("1")
+
 		LearnSessionForeignNative(c)
 	case 2:
-		//fmt.Println("2")
+
 		LearnSessionNativeForeign(c)
 	case 3:
-		//fmt.Println("3")
+
 		LearnSessionWriting(c)
 	case 4:
-		//fmt.Println("4")
+
 		LearnSessionPronunciation(c)
 	default:
 		LearnSessionForeignNative(c)
@@ -150,7 +84,7 @@ func LearnSession(c *fiber.Ctx) error {
 
 func LearnSessionForeignNative(c *fiber.Ctx) error {
 	activequestion, err := activequestiondb.GetActiveQuestionByUserID(db.DB, c.Locals("user_id").(int))
-	////fmt.Println(activequestion, err, "activequestion")
+
 	if err != nil {
 		if !strings.Contains(err.Error(), "no rows in result") {
 			return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja aktivne riječi activequestion", "link": "/dashboard"})
@@ -162,28 +96,26 @@ func LearnSessionForeignNative(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(400).SendString("Invalid ID")
 	}
-	// do the same thing but without setting the active queston and only randoming 3 words
+
 	_, words, err := userworddb.GetViableWordsForUserForDictionary(db.DB, c.Locals("user_id").(int), dictidInt)
 	if err != nil {
 		return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja riječi! nema rijeci za dict", "link": "/dashboard"})
 	}
 
 	finalWords := []worddb.Word{}
-	// add active question to final words
+
 	activeword, err := worddb.GetWordByID(db.DB, activequestion.WordID)
 	if err != nil {
 		return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja aktivne riječi!", "link": "/dashboard"})
 	}
 	finalWords = append(finalWords, activeword)
 	finalWords = fillWordList(words, finalWords, 3)
-	// swap values of foreign and native word from userWord
-	////fmt.Println("rijeci", finalWords)
+
 	for i := 0; i < len(finalWords); i++ {
 		finalWords[i].ForeignWord, finalWords[i].NativeWord = finalWords[i].NativeWord, finalWords[i].ForeignWord
 		finalWords[i].ForeignDescription, finalWords[i].NativeDescription = finalWords[i].NativeDescription, finalWords[i].ForeignDescription
 	}
-	////fmt.Println("rijeci", finalWords)
-	// randomize word order in finalWords
+
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(finalWords), func(i, j int) { finalWords[i], finalWords[j] = finalWords[j], finalWords[i] })
 	activeword.ForeignWord, activeword.NativeWord = activeword.NativeWord, activeword.ForeignWord
@@ -194,7 +126,7 @@ func LearnSessionForeignNative(c *fiber.Ctx) error {
 
 func LearnSessionNativeForeign(c *fiber.Ctx) error {
 	activequestion, err := activequestiondb.GetActiveQuestionByUserID(db.DB, c.Locals("user_id").(int))
-	////fmt.Println(activequestion, err)
+
 	if err != nil {
 		if !strings.Contains(err.Error(), "no rows in result") {
 			return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja aktivne riječi activequestion", "link": "/dashboard"})
@@ -207,25 +139,23 @@ func LearnSessionNativeForeign(c *fiber.Ctx) error {
 		return c.Status(404).Status(400).SendString("Invalid ID")
 	}
 
-	// do the same thing but without setting the active queston and only randoming 3 words
 	userWords, words, err := userworddb.GetViableWordsForUserForDictionary(db.DB, c.Locals("user_id").(int), dictidInt)
 	if err != nil {
 		return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja riječi! nema rijeci za dict", "link": "/dashboard"})
 	}
-	//////fmt.Println("rijeci", words)
-	////fmt.Println("userWords", userWords)
+
 	if len(userWords) < 4 {
 		return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Nema više riječi za učenje!", "link": "/dashboard"})
 	}
 	finalWords := []worddb.Word{}
-	// add active question to final words
+
 	activeword, err := worddb.GetWordByID(db.DB, activequestion.WordID)
 	if err != nil {
 		return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja aktivne riječi!", "link": "/dashboard"})
 	}
 	finalWords = append(finalWords, activeword)
 	finalWords = fillWordList(words, finalWords, 3)
-	// randomize word order in finalWords
+
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(finalWords), func(i, j int) { finalWords[i], finalWords[j] = finalWords[j], finalWords[i] })
 	return c.Render("learnSession", fiber.Map{"words": finalWords, "dictionaryId": dictidInt, "currentWord": activeword, "next": 3})
@@ -235,7 +165,7 @@ func LearnSessionNativeForeign(c *fiber.Ctx) error {
 func LearnSessionWriting(c *fiber.Ctx) error {
 
 	activequestion, err := activequestiondb.GetActiveQuestionByUserID(db.DB, c.Locals("user_id").(int))
-	////fmt.Println(activequestion, err)
+
 	if err != nil {
 		if !strings.Contains(err.Error(), "no rows in result") {
 			return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja aktivne riječi activequestion", "link": "/dashboard"})
@@ -248,7 +178,6 @@ func LearnSessionWriting(c *fiber.Ctx) error {
 		return c.Status(500).SendString("Invalid ID")
 	}
 
-	// get ative word
 	activeword, err := worddb.GetWordByID(db.DB, activequestion.WordID)
 	if err != nil {
 		return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja aktivne riječi!", "link": "/dashboard"})
@@ -259,7 +188,7 @@ func LearnSessionWriting(c *fiber.Ctx) error {
 
 func LearnSessionPronunciation(c *fiber.Ctx) error {
 	activequestion, err := activequestiondb.GetActiveQuestionByUserID(db.DB, c.Locals("user_id").(int))
-	////fmt.Println(activequestion, err)
+
 	if err != nil {
 		if !strings.Contains(err.Error(), "no rows in result") {
 			return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja aktivne riječi activequestion", "link": "/dashboard"})
@@ -270,7 +199,7 @@ func LearnSessionPronunciation(c *fiber.Ctx) error {
 	if err != nil {
 		return c.Status(500).SendString("Invalid ID")
 	}
-	// get ative word
+
 	activeword, err := worddb.GetWordByID(db.DB, activequestion.WordID)
 	if err != nil {
 		return c.Status(500).Render("forOfor", fiber.Map{"status": "500", "errorText": "Greška kod dohvaćanja aktivne riječi!", "link": "/dashboard"})
@@ -312,14 +241,14 @@ func CheckAnswer(c *fiber.Ctx) error {
 	var correct bool
 
 	if answerInt == activequestion.WordID {
-		// set current word as correct
+
 		err = userworddb.SetNewDelayForUser(db.DB, c.Locals("user_id").(int), activequestion.WordID, 1)
 		if err != nil {
 			return c.Status(500).SendString("Greška kod postavljanja nove riječi!")
 		}
 		correct = true
 	} else {
-		// set current word as correct
+
 		err = userworddb.SetNewDelayForUser(db.DB, c.Locals("user_id").(int), activequestion.WordID, 0)
 		if err != nil {
 			return c.Status(500).SendString("Greška kod postavljanja nove riječi!")
@@ -362,14 +291,14 @@ func CheckWritingAnswer(c *fiber.Ctx) error {
 	activeword.ForeignWord = strings.ToLower(activeword.ForeignWord)
 	fmt.Println(answer, activeword.ForeignWord)
 	if answer == activeword.ForeignWord {
-		// set current word as correct
+
 		err = userworddb.SetNewDelayForUser(db.DB, c.Locals("user_id").(int), activequestion.WordID, 1)
 		if err != nil {
 			return c.Status(500).SendString("Greška kod postavljanja nove riječi!")
 		}
 		correct = true
 	} else {
-		// set current word as correct
+
 		err = userworddb.SetNewDelayForUser(db.DB, c.Locals("user_id").(int), activequestion.WordID, 0)
 		if err != nil {
 			return c.Status(500).SendString("Greška kod postavljanja nove riječi!")
@@ -399,19 +328,18 @@ func CheckListeningAnswer(c *fiber.Ctx) error {
 		}
 	}
 
-	// random a valute 0 to 100
 	rand.Seed(time.Now().UnixNano())
 	random := rand.Intn(100)
 	var correct bool
 	if random > 50 {
-		// set current word as correct
+
 		err = userworddb.SetNewDelayForUser(db.DB, c.Locals("user_id").(int), activequestion.WordID, 1)
 		if err != nil {
 			return c.Status(500).SendString("Greška kod postavljanja nove riječi!")
 		}
 		correct = true
 	} else {
-		// set current word as correct
+
 		err = userworddb.SetNewDelayForUser(db.DB, c.Locals("user_id").(int), activequestion.WordID, 0)
 		if err != nil {
 			return c.Status(500).SendString("Greška kod postavljanja nove riječi!")
